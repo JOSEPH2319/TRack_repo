06230f80d687f4c8509dae930c0caafc71da139d
JOSEPH2319 <54958447+JOSEPH2319@users.noreply.github.com>
2020-05-17 19:43:01 -0400

    Create track.py

---
+++ track.py
@@ -0,0 +1,8 @@
+# "cook" short for chinook, just trying to be creative IDK
+cook = http://sqliteviewer.flowsoft7.com/?state=%7B%22ids%22:%5B%22139hxcE7GtruLjULNzWsv1TQtxDrqxsVM%22%5D,%22action%22:%22open%22,%22userId%22:%22112672611949326017192%22%7D
+
+
+DELETE * FROM cook
+WHERE Grunge IS NULL;
+
+SELECT * FROM;
